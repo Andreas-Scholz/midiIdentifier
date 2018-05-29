38ecf9aaad1e59f7f81867cebeb91b6ad7cedac1
Schick <karsten.schick@gmail.com>
2018-05-29 12:27:56 +0200

    Added compare module to compare a new midi-string to an archive of midi-strings and return a ratio two which the strings match

---
+++ src/comparator/compare.py
@@ -0,0 +1,8 @@
+import difflib
+
+def do_match (input, archive):
+    matches = dict()
+    for key, value in archive.items():
+        ratio = difflib.SequenceMatcher(None, input, value).ratio()
+        matches[key] = "%.2f" % round(ratio*100,2)
+    return matches
