d77f74bbb14ac2879fdd5c30b2894890c277f472
Schick <karsten.schick@gmail.com>
2018-06-20 17:41:48 +0200

    trying to fix system recording after 100% is reached

--- src/midiSourceMI/piano.py
+++ src/midiSourceMI/piano.py
@@ -7,7 +7,7 @@ from telnetlib import Telnet
 # set up pygame
 pygame.mixer.pre_init(44100, -16, 2, 2048)
 pygame.init()
-#pygame.midi.init()
+pygame.midi.init()
 
 # set up fluidsynth
 #fluid = Telnet("192.168.0.21","9988")
@@ -24,7 +24,6 @@ class Piano(object):
     midi = ""
 
     def __init__(self, input_device_id):
-        pygame.midi.init()
         self.inp = pygame.midi.Input(input_device_id)
         self._reset_all()
 
@@ -70,8 +69,8 @@ class Piano(object):
                     self.progress += 5
             # wait a short while to prevent 100% cpu utilization
             pygame.time.wait(100)
+        self.inp.read()
         self.inp.close()
-        pygame.midi.quit()
         self.isDone = True
 
 def main():
