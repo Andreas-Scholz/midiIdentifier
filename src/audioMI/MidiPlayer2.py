cc4a20140aba052918c90517f2cb9ec1e0c7cbe6
Schick <karsten.schick@gmail.com>
2018-06-20 15:20:59 +0200

    Added new version of MidiPlayer (testing stage)

--- src/audioMI/MidiPlayer2.py
+++ src/audioMI/MidiPlayer2.py
@@ -1,18 +1,20 @@
 import time
-import threading
+from telnetlib import Telnet
 import subprocess
 
 class MidiPlayer2():
 
     def __init__(self):
         self.p = None
+        self.fluid = Telnet("192.168.0.21","9988")
 
     def play(self, file):
         self.p = subprocess.Popen(["aplaymidi", "-p128:0", file])
 
     def stop(self):
         self.p.terminate()
-
+        self.fluid.write("reset")
+        self.fluid.write("quit")
 
 def main():
     player = MidiPlayer2()
